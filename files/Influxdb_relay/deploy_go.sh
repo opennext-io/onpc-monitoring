af943ba0e58750ca91ea6d0d243d12c76145484c
Patrick Petit <ppetit@mirantis.com>
2018-03-19 12:26:40 +0100

    Refactored influxdb_relay playbook

--- files/Influxdb_relay/deploy_go.sh
+++ files/Influxdb_relay/deploy_go.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pushd /opt
-  wget -P /opt/ https://storage.googleapis.com/golang/go1.7.3.linux-amd64.tar.gz
-  tar  -xzf /opt/go1.7.3.linux-amd64.tar.gz -C /opt/
+  wget -P /opt/ https://dl.google.com/go/go1.10.linux-amd64.tar.gz
+  tar  -xzf /opt/go1.10.linux-amd64.tar.gz -C /opt/
 popd
 pushd /usr/local/bin
   find /opt/go/bin/ -type f -exec ln -sf {} \;
